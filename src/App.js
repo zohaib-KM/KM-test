c6f54d867b0f129334cbea4e85f1900f1100cffd
unknown <zohaib.hamdani@kryptomind.com>
2024-05-23 11:01:54 +0500

    changed app

--- src/App.js
+++ src/App.js
@@ -10,8 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload!!!!.
         </p>
         <p>
-          This is a sample react app!!!!!
-        </p>
+          This is a change code app       </p>
         <p>
           This is a sample react app!
         </p>
